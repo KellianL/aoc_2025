ae5dc0a053923d39793530e883cfba9fb22549fd
KellianL <kellian1990@hotmail.fr>
2025-12-04 10:33:07 +0100

    :santa: Day 3

---
+++ src/bin/03.rs
@@ -0,0 +1,89 @@
+advent_of_code::solution!(3);
+
+fn find_max_two_digits(s: &str) -> u64 {
+    let b = s.as_bytes();
+    let mut iter = b.iter().rev();
+    let mut max = *iter.next().unwrap();
+    let (mut unite, mut dizaine) = (0_u8, 0_u8);
+    for &curr in iter {
+        if curr > dizaine || (curr == dizaine && max > unite) {
+            dizaine = curr;
+            unite = max;
+        }
+        if curr > max {
+            max = curr;
+        }
+    }
+    ((dizaine - b'0') as u64) * 10 + ((unite - b'0') as u64)
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    Some(input.lines().map(find_max_two_digits).sum())
+}
+
+fn find_max_n_digits(s: &str, n: usize) -> Option<u64> {
+    let b = s.as_bytes();
+
+    let mut best = vec![None; n + 1];
+    let mut powers = vec![1u64; n];
+    for i in 1..n {
+        powers[i] = powers[i - 1] * 10;
+    }
+
+    for &c in b.iter().rev() {
+        let digit = (c - b'0') as u64;
+
+        for k in (2..=n).rev() {
+            if let Some(prev_val) = best[k - 1] {
+                let candidate = digit * powers[k - 1] + prev_val;
+
+                match best[k] {
+                    Some(current_max) => {
+                        if candidate > current_max {
+                            best[k] = Some(candidate);
+                        }
+                    }
+                    None => {
+                        best[k] = Some(candidate);
+                    }
+                }
+            }
+        }
+
+        match best[1] {
+            Some(current_max) => {
+                if digit > current_max {
+                    best[1] = Some(digit);
+                }
+            }
+            None => best[1] = Some(digit),
+        }
+    }
+    best[n]
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    Some(
+        input
+            .lines()
+            .filter_map(|line| find_max_n_digits(line, 12))
+            .sum(),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
